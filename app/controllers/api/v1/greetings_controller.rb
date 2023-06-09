279f4bccb002532b0d6ae3b7d4a060e5d7d926e5
Choice Osobor <choiceosobor@gmail.com>
2023-06-09 16:23:07 +0100

    create greetings in db from seed

--- app/controllers/api/v1/greetings_controller.rb
+++ app/controllers/api/v1/greetings_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::GreetingsController < ApplicationController
   def index
+    num = rand(1..Greeting.count)
     response.headers['Access-Control-Allow-Origin'] = '*'
-    render json: Greeting.all.sample
+    render json: Greeting.find(num)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Greeting.destroy_all
 Greeting.create(greeting: 'Hello!')
 Greeting.create(greeting: 'Bonjour!')
 Greeting.create(greeting: 'Hola!')
